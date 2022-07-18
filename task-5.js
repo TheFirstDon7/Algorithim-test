30e39d0ec223bd85d694d64f9f9972404ec8d3da
TheFirstDon7 <samaigbiriocasey7@gmail.com>
2022-07-18 08:10:11 +0100

    second commit

---
+++ task-5.js
@@ -0,0 +1,7 @@
+var fullName = "Casey Sam Aigbirio ";
+
+fullName = fullName.trim(); //use .trim to remove all whitespace
+
+fullName = fullName.replaceAll(" ", "%20"); // use.replace all to replace all whitespace with %20
+
+console.log(fullName);
