777138bd366d04645f4c673444b1b39bb6f2a353
TheFirstDon7 <samaigbiriocasey7@gmail.com>
2022-07-18 09:00:00 +0100

    third commit

--- task-2.js
+++ task-2.js
@@ -8,7 +8,8 @@ function prime_numbers(n) {
         return true;
     } else {
         for (var x = 2; x < n; x++) {
-            if (n % x === 0) {
+            if (n % x === 0) { //using the modulo operator to divide n by x and if there is a reminder print out false
+                //since x must be greater than n 1%2=0.5 which is greater than 0 print false 
                 return false;
             }
         }
